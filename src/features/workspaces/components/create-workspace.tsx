fc1053fe58ff6f2abb3bd9e8e13af49ce17f76fb
jayc595 <jay.crocker@hotmail.co.uk>
2024-08-26 19:35:38 +0100

    Started saving of workspaces to db

--- src/features/workspaces/components/create-workspace.tsx
+++ src/features/workspaces/components/create-workspace.tsx
@@ -1,20 +1,33 @@
 "use client";
 
 import React from 'react'
-import { useCreateWorkspaceModal } from '../use-create-workspace-modal'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { useCreateWorkspace } from '../use-create-workspace';
+import { useCreateWorkspaceModal } from '../use-create-workspace-modal';
 
 type Props = {}
 
 const CreateWorkspace = (props: Props) => {
     const [open, setOpen] = useCreateWorkspaceModal();
 
+    const { mutate } = useCreateWorkspace();
+
     const handleClose = () => {
         setOpen(false);
         //TODO: clear form.
     }
+
+    const handleSubmit = () => {
+        mutate({
+            name: "Workspace 1",
+        }, {
+            onSuccess(data){
+                // Redirect to workspace.
+            }
+        })
+    };
 
   return (
     <Dialog open={open} onOpenChange={handleClose}>
