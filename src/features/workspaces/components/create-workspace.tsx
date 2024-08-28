9d556b1d25d34872b4249392992d519612a4d2a9
jayc595 <jay.crocker@hotmail.co.uk>
2024-08-28 21:17:06 +0100

    Allows for saving of workspace to convex

--- src/features/workspaces/components/create-workspace.tsx
+++ src/features/workspaces/components/create-workspace.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -11,20 +11,22 @@ type Props = {}
 
 const CreateWorkspace = (props: Props) => {
     const [open, setOpen] = useCreateWorkspaceModal();
+    const [name, setName] = useState("");
 
-    const { mutate } = useCreateWorkspace();
+    const { mutate, isPending, isError, isSuccess, data, error } = useCreateWorkspace();
 
     const handleClose = () => {
         setOpen(false);
         //TODO: clear form.
     }
 
-    const handleSubmit = () => {
-        mutate({
-            name: "Workspace 1",
-        }, {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        mutate({ name }, {
             onSuccess(data){
-                // Redirect to workspace.
+                console.log(data);
+                setOpen(false);
             }
         })
     };
@@ -35,11 +37,11 @@ const CreateWorkspace = (props: Props) => {
             <DialogHeader>
                 <DialogTitle>Create a new workspace</DialogTitle>
             </DialogHeader>
-            <form className='space-y-4'>
+            <form onSubmit={handleSubmit} className='space-y-4'>
                 {/* @TODO: Add more to initial form, ability to upload photo. */}
-                <Input disabled={false} required autoFocus minLength={3} placeholder='Workspace name'></Input>
+                <Input disabled={isPending} value={name} onChange={(e) => setName(e.target.value)} required autoFocus minLength={3} placeholder='Workspace name'></Input>
                 <div className='flex justify-end'>
-                    <Button disabled={false}>Create</Button>
+                    <Button disabled={isPending}>Create</Button>
                 </div>
             </form>
         </DialogContent>
